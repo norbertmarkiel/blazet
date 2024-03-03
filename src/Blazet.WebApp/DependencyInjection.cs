7637fdbcc3d2e8dba7689afe6607c8ffa0ff7e4e
NorbertMarkiel <norbertmarkiel@gmail.com>
2024-03-03 08:45:57 +0100

    Added Mudblazor

--- src/Blazet.WebApp/DependencyInjection.cs
+++ src/Blazet.WebApp/DependencyInjection.cs
@@ -1,9 +1,12 @@
-﻿namespace Blazet.WebApp
+﻿using MudBlazor.Services;
+
+namespace Blazet.WebApp
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddWebApp(this IServiceCollection services, IConfiguration config)
         {
+            services.AddMudServices();
             return services;
 
         }
