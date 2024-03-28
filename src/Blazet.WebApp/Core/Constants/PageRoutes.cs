7898b48a1e61413e813756ca299dcfbb0e142bfc
NorbertMarkiel <norbertmarkiel@gmail.com>
2024-03-28 20:21:44 +0100

    Refactor

--- src/Blazet.WebApp/Core/Constants/PageRoutes.cs
+++ src/Blazet.WebApp/Core/Constants/PageRoutes.cs
@@ -3,5 +3,12 @@
     public static class PageRoutes
     {
         public const string OrderList = "orders";
+
+
+        public const string Login = "/login";
+        public const string Logout = "/logout";
+
+        public const string Home = "/home";
+
     }
 }
