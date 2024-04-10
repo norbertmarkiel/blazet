8b9bfba7a7bc9762a4736cb8cc8e88bfa7fb0a24
Norbert <norbertmarkiel@gmail.com>
2024-04-10 20:38:14 +0200

    Test_Base

---
+++ Application.IntegrationTests/TestBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.IntegrationTests
+{
+    using static TestsSetup;
+    public class TestBase
+    {
+        [SetUp]
+        public async Task TestSetUp()
+        {
+            await ResetState();
+        }
+    }
+}
